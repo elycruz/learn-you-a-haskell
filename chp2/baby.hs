32a3e4174b1e0bf6d7f046c447f94d9d866b3b3b
Ely De La Cruz <elycruz@elycruz.com>
2018-02-12 21:01:47 -0500

    chp2 - Added type notations to definitions in file.

--- chp2/baby.hs
+++ chp2/baby.hs
@@ -1,22 +1,32 @@
+alphabet :: [Char]
+alphabet = ['a'..'z']
 
+doubleMe :: Num a => a -> a
 doubleMe x = x + x
 
+doubleUs :: Num a => a -> a -> a
 doubleUs x y = doubleMe x + doubleMe y
 
+doubleSmallNumber :: (Ord a, Num a) => a -> a
 doubleSmallNumber x = if x > 100 then x else x * 2
 
+doubleSmallNumber' :: (Ord a, Num a) => a -> a
 doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
 
+conanO'Brien :: [Char]
 conanO'Brien = "It's a-me, Conan O'Brien!"
 
+boomBangs :: Integral a => [a] -> [(a, [Char])]
 boomBangs xs = [ if x < 10 then (x, "BOOM!") else (x, "BANG!") | x <- xs, odd x ]
 
+length' :: (Num b) => [a] -> b
 length' xs = sum [ 1 | _ <- xs ]
 
-removeNonUpperCase str = [ char | char <- str, not $ elem char ['A'..'Z'] ]
+removeNonUpperCase :: [Char] -> [Char]
+removeNonUpperCase str = [ char | char <- str, not $ elem char $ alphabet ]
 
 removeNonUpperCase' :: [Char] -> [Char]
-removeNonUpperCase' st = [ c | c <- st, not $ elem c ['A'..'Z']]
+removeNonUpperCase' st = [ c | c <- st, not $ elem c alphabet]
 
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
@@ -27,5 +37,5 @@ factorial n = product [1..n]
 circumference :: Float -> Float
 circumference r = 2 * pi * r
 
-circum' :: Double -> Double
-circum' r = 2 * pi * r
+circumf' :: Double -> Double
+circumf' r = 2 * pi * r
